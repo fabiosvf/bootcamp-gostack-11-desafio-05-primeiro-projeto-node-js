2db6ac06824360d9b3003211ac9d72f954be90a8
Fabio Silva Ferreira <fabiosvf@gmail.com>
2020-07-31 03:34:58 -0300

    Desafio 05 - Primeiro Projeto Node.js - Finalizando o desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,20 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface Result {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +24,40 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
+  public all(): Result {
     // TODO
+    const balance = this.getBalance();
+
+    const result: Result = {
+      transactions: this.transactions,
+      balance,
+    };
+
+    return result;
   }
 
   public getBalance(): Balance {
     // TODO
+    const income = this.transactions.reduce((subtotal, item) => {
+      return subtotal + (item.type === 'income' ? item.value : 0);
+    }, 0);
+
+    const outcome = this.transactions.reduce((subtotal, item) => {
+      return subtotal + (item.type === 'outcome' ? item.value : 0);
+    }, 0);
+
+    const balance = { income, outcome, total: income - outcome };
+
+    return balance;
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
